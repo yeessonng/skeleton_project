9c83f99be3e6084af8010441365c34d5ed49241c
yeessonng <dorlek856@gmail.com>
2025-04-14 15:34:59 +0900

    vo파일 추가

--- src/main/java/TravelVO.java
+++ src/main/java/TravelVO.java
@@ -1,2 +1,67 @@
 public class TravelVO {
+    private int id;
+    private String district;
+    private String description;
+    private String address;
+    private String phone;
+
+    public TravelVO(int id, String district, String address, String description, String phone) {
+        this.id = id;
+        this.district = district;
+        this.address = address;
+        this.description = description;
+        this.phone = phone;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "TravelVO{" +
+                "id=" + id +
+                ", district='" + district + '\'' +
+                ", description='" + description + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
