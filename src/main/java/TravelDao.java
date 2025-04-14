a605aa84fb78b46f0703b7229152993e2e51c064
yeessonng <dorlek856@gmail.com>
2025-04-14 15:42:50 +0900

    dao 코드 추가

--- src/main/java/TravelDao.java
+++ src/main/java/TravelDao.java
@@ -1,2 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TravelDao {
+    private Connection conn;
+
+    public TravelDao() {
+        String url = "jdbc:mysql://localhost:3306/travel_db";
+        String user = "root";
+        String password = "98653232";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver" );
+            this.conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println( "[에러] " + e.getMessage() );
+        } catch (SQLException e) {
+            System.out.println( "[에러] " + e.getMessage() );
+        }
+    }
+
+    //
+
 }
